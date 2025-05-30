215284ebd22e041505f192bb2aa0a7ec4f9e919d
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2025-05-30 15:53:30 -0400

    Use metaobject for banner message

--- extensions/payment-banner/src/Checkout.jsx
+++ extensions/payment-banner/src/Checkout.jsx
@@ -1,49 +1,51 @@
-import '@shopify/ui-extensions/preact';
 import {render} from "preact";
-import {useSubscription} from "@shopify/ui-extensions/checkout/preact"
+import {useEffect, useState} from 'preact/hooks';
+import {useApi, useSelectedPaymentOptions} from "@shopify/ui-extensions/checkout/preact"
 
-// 1. Export the extension
 export default function() {
   render(<Extension />, document.body)
 }
 
 function Extension() {
-  const instructions = useSubscription(shopify.instructions);
+  const [data, setData] = useState();
+  const {query} = useApi();
 
-  // 2. Check instructions for feature availability, see https://shopify.dev/docs/api/checkout-ui-extensions/apis/cart-instructions for details
-  if (!instructions.attributes.canUpdateAttributes) {
-    // For checkouts such as draft order invoices, cart attributes may not be allowed
-    // Consider rendering a fallback UI or nothing at all, if the feature is unavailable
-    return (
-      <s-banner heading="payment-banner" tone="warning">
-        {shopify.i18n.translate("attributeChangesAreNotSupported")}
-      </s-banner>
-    );
-  }
+  const paymentMethods = useSelectedPaymentOptions();
 
-  // 3. Render a UI
-  return (
-    <s-banner heading="payment-banner">
-      <s-stack gap="base">
-        <s-text>
-          {shopify.i18n.translate("welcome", {
-            target: <s-text type="emphasis">{shopify.extension.target}</s-text>,
-          })}
-        </s-text>
-        <s-button onClick={handleClick}>
-          {shopify.i18n.translate("addAFreeGiftToMyOrder")}
-        </s-button>
-      </s-stack>
-    </s-banner>
+  useEffect(() => {
+    query(
+      `query {
+        metaobjects(first: 10, type: "$app:payment_messages") {
+          nodes {
+            id
+            fields {
+              key
+              value
+            }
+          }
+        }
+      }`,
+      {
+        version: "2025-07"
+      }
+    )
+      .then(({data, errors}) => setData(data))
+      .catch(console.error);
+  }, [query]);
+
+  const paymentMessages = data?.metaobjects?.nodes?.map(node => {
+    const message = {
+      id: node.id
+    }
+    node.fields.forEach(field => {
+      message[field.key] = field.value;
+    });
+    return message;
+  });
+
+  const displayMessages = paymentMessages?.filter(message =>
+    paymentMethods.findIndex(paymentMethod => message.payment_type === paymentMethod.type) !== -1
   );
 
-  async function handleClick() {
-    // 4. Call the API to modify checkout
-    const result = await shopify.applyAttributeChange({
-      key: "requestedFreeGift",
-      type: "updateAttribute",
-      value: "yes",
-    });
-    console.log("applyAttributeChange result", result);
-  }
+  return displayMessages?.map(message => <s-banner heading={message.message} id={message.id}></s-banner>) || null;
 }
