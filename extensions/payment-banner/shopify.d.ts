98bf3612601d9baa5166730ae4ee8fb2d81d3dcd
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2025-05-30 15:45:37 -0400

    Update target to purchase.checkout.payment-method-list.render-before

--- extensions/payment-banner/shopify.d.ts
+++ extensions/payment-banner/shopify.d.ts
@@ -2,6 +2,6 @@ import '@shopify/ui-extension';
 
 //@ts-ignore
 declare module './src/Checkout.jsx' {
-  const shopify: import('@shopify/ui-extensions/purchase.checkout.block.render').Api;
+  const shopify: import('@shopify/ui-extensions/purchase.checkout.payment-method-list.render-before').Api;
   const globalThis: { shopify: typeof shopify };
 }
